ba8c2520509c89c1e95333d7e5a0a0bbc7e2dff7
Johng117 <83654331+Johng117@users.noreply.github.com>
2024-02-11 14:55:14 +0000

    updated database schema

--- server/data/public_schema.sql
+++ server/data/public_schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS public.users CASCADE;
+DROP TABLE IF EXISTS public.images CASCADE;
 
-CREATE TABLE IF NOT EXISTS public.users (
+CREATE TABLE IF NOT EXISTS public.users(
     user_id int UNIQUE GENERATED ALWAYS AS IDENTITY,
     first_name text,
     last_name text,
@@ -9,6 +10,16 @@ CREATE TABLE IF NOT EXISTS public.users (
     is_admin boolean DEFAULT false,
     created_at timestamp NOT NULL DEFAULT NOW(),
     updated_at timestamp,
-    CONSTRAINT users_pkey PRIMARY KEY(user_id),
+    CONSTRAINT users_pkey PRIMARY KEY(user_id)
+)
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public.images(
+    image_id int UNIQUE GENERATED ALWAYS AS IDENTITY,
+    image_description text,
+    created_by int,
+    created_at timestamp NOT NULL DEFAULT NOW(),
+    CONSTRAINT images_pkey PRIMARY KEY(image_id),
+    CONSTRAINT images_user_fkey FOREIGN KEY(created_by) REFERENCES public.users(user_id) 
 )
 TABLESPACE pg_default;
